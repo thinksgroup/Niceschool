754b0827cbf1c8cf54223fbec3d1024688c15ef3
jkunkun <2426632201@qq.com>
2019-11-12 23:16:51 +0800

    焦振坤----教师查询学生成绩

---
+++ Niceschool/niceschoolapi/src/main/java/com/nsapi/niceschoolapi/controller/TchSelStuExamController.java
@@ -0,0 +1,39 @@
+package com.nsapi.niceschoolapi.controller;
+
+import com.nsapi.niceschoolapi.common.config.MySysUser;
+import com.nsapi.niceschoolapi.entity.LayuiResult;
+import com.nsapi.niceschoolapi.entity.TchSelStuExamVO;
+import com.nsapi.niceschoolapi.service.TchSelStuExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class TchSelStuExamController {
+    @Autowired
+    private TchSelStuExamService tchSelStuExamService;
+
+    @RequestMapping("/tchSelExam")
+    public String tchSelExam(Model model,TchSelStuExamVO tchSelStuExamVO){
+        String tid=MySysUser.loginName();
+        List<Map> tchClass = tchSelStuExamService.selTchClass(tid);
+        model.addAttribute("tchClass",tchClass);
+        return "view/student/tchSelStuExam";
+    }
+
+    @RequestMapping("/tchSelStuExam")
+    @ResponseBody
+    public LayuiResult<Map> tchSelStuExam(TchSelStuExamVO tchSelStuExamVO){
+        String tid=MySysUser.loginName();
+        tchSelStuExamVO.setTid(tid);
+        List<Map> exam = tchSelStuExamService.tchSelStuExam(tchSelStuExamVO);
+        LayuiResult<Map> result = new LayuiResult<>();
+        result.setData(exam);
+        return result;
+    }
+}
---
+++ Niceschool/niceschoolapi/src/main/java/com/nsapi/niceschoolapi/service/TchSelStuExamService.java
@@ -0,0 +1,13 @@
+package com.nsapi.niceschoolapi.service;
+
+import com.nsapi.niceschoolapi.entity.TchSelStuExamVO;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TchSelStuExamService {
+    //查询教师授课班级
+    List<Map> selTchClass(String tid);
+    //教师查询学生成绩
+    List<Map> tchSelStuExam(TchSelStuExamVO tchSelStuExamVO);
+}
