9af6d60b3a8af913ea6b7a74a93f8b433528af57
Mr946 <525069428@qq.com>
2019-11-18 02:39:47 +0800

    完成部分教师授课管理

--- Niceschool/niceschoolapi/src/main/java/com/nsapi/niceschoolapi/service/impl/TchCourseServiceImpl.java
+++ Niceschool/niceschoolapi/src/main/java/com/nsapi/niceschoolapi/service/impl/TchCourseServiceImpl.java
@@ -24,5 +24,45 @@ public class TchCourseServiceImpl implements TchCourseService {
     public List<TeacherDB> findAllTchByName() {
         return tchCourseMapper.findAllTchByName();
     }
+    //通过专业查询全部课程
+    @Override
+    public List<TeacherDB> findAllCourseByMajorName(TchCourseVO tchCourseVO) {
+        return tchCourseMapper.findAllCourseByMajorName(tchCourseVO);
+    }
+    //查询级联班级
+    @Override
+    public List<TeacherDB> findTchCourseClassName(ClassinfoDB classinfoDB) {
+        return tchCourseMapper.findTchCourseClassName(classinfoDB);
+    }
+    //查询周
+    @Override
+    public List<WeeksDB> findAllWeeks() {
+        return tchCourseMapper.findAllWeeks();
+    }
+    //查询节段
+    @Override
+    public List<ScheduleDB> findAllSchedule() {
+        return tchCourseMapper.findAllSchedule();
+    }
+    //添加教师授课表记录并返回新数据id
+    @Override
+    public int addOneTchCourse(TchCourseVO tchCourseVO) {
+        return tchCourseMapper.addOneTchCourse(tchCourseVO);
+    }
+    //添加教师授班表记录
+    @Override
+    public int addOneTchClass(TchCourseVO tchCourseVO) {
+        return tchCourseMapper.addOneTchClass(tchCourseVO);
+    }
+
+    @Override
+    public int findTchCourseFlag(TchCourseVO tchCourseVO) {
+        return tchCourseMapper.findTchCourseFlag(tchCourseVO);
+    }
+    //查询指定老师教授所有课程
+    @Override
+    public List<TchCourseVO> findCourseByTch(TchCourseVO tchCourseVO) {
+        return tchCourseMapper.findCourseByTch(tchCourseVO);
+    }
 
 }
